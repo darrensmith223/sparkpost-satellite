38e4b9a140af2be5974ad04e85fb72256c572ec9
dsmith <darrensmith223@comcast.net>
2021-07-08 11:14:31 -0400

    Add testing

---
+++ test/test_sparkpostsatellite.py
@@ -0,0 +1,18 @@
+import sparkpostsatellite
+import responses
+
+
+@responses.activate
+def test_get_customer_id():
+    api_url = "https://api.sparkpost.com/api/v1/account"
+    responses.add(
+        responses.GET,
+        api_url,
+        status=200,
+        content_type='application/json',
+        body='{"results": "yay"}'
+    )
+
+    # get customer_id
+    api_key = "fake-key"
+    response = sparkpostsatellite.get_customer_id(api_key)
